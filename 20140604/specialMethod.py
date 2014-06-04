5e721a68ba37e877ac03868327b6502fff39df05
fl0wjacky <flowjacky@gmail.com>
2014-06-04 23:15:52 +0800

    add context manager上下文管理器

--- 20140604/specialMethod.py
+++ 20140604/specialMethod.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # coding:utf-8
-# 特殊方法名的前后各有两个下划线
+# 特殊方法名的前后各有两个下划线,特殊方法又被称为魔法方法(magic method),定义了许多Python语法和表达方式。可通过dir()查看
 
 print 'abc' + 'xyz'
 print 'abc'.__add__('xyz')
@@ -19,3 +19,19 @@ print li[3]
 print li.__getitem__(3)
 li.__setitem__(0,10)
 print li
+
+di = {'a':1, 'b':2}
+print di
+di.__delitem__('a')
+print di
+del di['b']
+print di
+
+# 在Python中，函数也是一种对象。实际上，任何一个有__call__()特殊方法的对象都被当作是函数。
+class SampleMore(object):
+	def __call__(self, a):
+		return a+5
+
+add = xx()
+print(add(2))
+print map(add, [2, 4, 5])
